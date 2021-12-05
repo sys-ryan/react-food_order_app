362c51594b8de38bb344988ee16ced7e1d4087c2
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-12-05 17:55:14 +0900

    feat: create CartButton component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import HeaderCartButton from "./HeaderCartButton";
+
 import mealsImage from "../../assets/meals.jpeg";
 
 import classes from "./Header.module.css";
@@ -9,7 +11,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} />
